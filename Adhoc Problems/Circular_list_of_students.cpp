30a13375c7f4cbb55b81fe6160760be0021312e1
Deepanshu Udhwani <deepanshu1422@gmail.com>
2020-06-01 20:54:11 +0530

    Update Circular_list_of_students.cpp

--- Adhoc Problems/Circular_list_of_students.cpp
+++ Adhoc Problems/Circular_list_of_students.cpp
@@ -6,7 +6,7 @@ First line will have an integer ‘t’, denoting the number of test cases.
 Next line will have two space separated integers denoting the value of ‘i’ and ‘p’ respectively.
 Output Format:
 Print ‘t’ lines containing single integer denoting the student number.
-Constraints:
+Constraints://
 1 <= t <= 10^5
 0 <= i <= 11
 1 <= p <= 12
