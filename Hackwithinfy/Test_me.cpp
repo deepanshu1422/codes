1c0af59f4bed5bdad4d20f2b4c022e5df9ffc0d6
Deepanshu Udhwani <deepanshu1422@gmail.com>
2020-06-01 20:52:04 +0530

    Update Test_me.cpp

--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<unordered_map>
-#define fast                          \
+#define fast                          \//
     ios_base::sync_with_stdio(false); \
     cin.tie(0);
 #define int long long int
