62402ecb0ee830a7d5c29e2b5969782793752cbe
Deepanshu Udhwani <deepanshu1422@gmail.com>
2020-06-01 20:59:03 +0530

    Update Circular_list_of_students.cpp

--- Adhoc Problems/Circular_list_of_students.cpp
+++ Adhoc Problems/Circular_list_of_students.cpp
@@ -1,7 +1,7 @@
 /* You are given a circular list of students as follows:
 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 11
 This list is circular, means that 11 will follow 0 again. You will be g///iven the student number ‘i’ and some position ‘p’. You will have to tell that if the list will start from (i+1)th student, then which student will be at pth position.
-Input Format://
+Input Format://///
 First line will have an integer ‘t’, denoting the number of test cases.
 Next line will have two space separated integers denoting the value of ‘i’ and ‘p’ ///respectively.
 Output Format:
