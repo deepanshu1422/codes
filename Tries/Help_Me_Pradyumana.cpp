afe7862d914f7154d6f4070e662063e9ce4db2dc
Deepanshu Udhwani <deepanshu1422@gmail.com>
2020-06-04 01:01:21 +0600

    Update Help_Me_Pradyumana.cpp

--- Tries/Help_Me_Pradyumana.cpp
+++ Tries/Help_Me_Pradyumana.cpp
@@ -1,5 +1,5 @@
 /* Pradyumn is tired of using auto - correct feature in his smartphone. He needs to correct his auto - correct more times than the auto - correct corrects him. Pradyumn is thinking to make his own app for mobile which will restrict auto - correct feature, instead it will provide auto - completion. Whenever Pradyumn types factorial, auto - correct changes it to fact. Pradyumn's App will show options such as fact, factorial, factory. Now, he can chose from any of these options. As Pradyumn is busy with his front - end work of his App. He asks you to help him. He said "You will be given set of words(words may repeat too but counted as one only). Now, as user starts the app, he will make queries(in lowercase letters only). So, you have to print all the words of dictionary which have the prefix same as given by user as input. And if no such words are available, add this word to your dictionary." As you know, Pradyumn want this app to be as smart as him :P So, implement a program for him such that he can release it on Fun Store.
-INPUT CONSTRAINTS
+INPUT CONSTRAINTS//
 1≤N≤30000
 sum(len(string[i]))≤2∗10^5
 1≤Q≤10^3
